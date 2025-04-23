ea0cb2ae628e92ceb52617b3aebae60e1725f867
EduardoBezerraCg <95935305+EduardoBezerraCg@users.noreply.github.com>
2025-04-23 23:13:26 +0200

    Commit of the day

---
+++ dags/ML/ML1.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from airflow import DAG
+import requests
+import pandas as pd
+import os
+
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+
+
+dagName = "ML 1"
+
+AIRFLOW_HOME = '/opt/airflow/clickboard'
+
+
+with DAG(
+    dag_id=f'{dagName}',  # Name of the DAG
+    schedule_interval=None,  # Manual trigger
+    catchup=False,  # No backfilling
+    start_date=datetime(2021, 1, 1),
+    tags=[f"{dagName}", "ETL Portfolio"]
+) as dag:
+    pass
